30ca9e5156359e29ef399d1c25c4c1837e8ed5db
Sebuntu <sebastiangomezzz@gmail.com>
2022-09-19 12:21:14 +0200

    set up folder, views, and navigation structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import OpenbankLogo from "./assets/img/key_openbank.png";
+import { Step1, Step2, Step3 } from "./views";
+import { Layout } from "./components/common";
 
 import "./App.scss";
 
 export const App = () => {
-  return <div>Hola</div>;
+  return (
+    <Layout>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Step1 />} />
+          <Route path="/create_password" element={<Step2 />} />
+          <Route path="/feedback" element={<Step3 />} />
+        </Routes>
+      </Router>
+    </Layout>
+  );
 };
